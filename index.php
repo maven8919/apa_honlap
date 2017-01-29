ed34221ae73f0dffbb1cbe1aeb69c9b94aaa0cf3
maven8919 <maven8919@gmail.com>
2017-01-29 16:05:06 +0100

    Add todo list

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php include_once("home.html"); ?>
+
+// Családfa
+// Szavazás: Mi volt Imre herceg halálának az oka?
+//	            Betegség
+//              Vaddisznó baleset
+//              Kabán /Vadkan/ besenyő törzs
+//              Nem tudom
+//
+// Örülni szabad? 2 kép: top 10, 1.hely
+//							Első nyomtatott példánnyal
